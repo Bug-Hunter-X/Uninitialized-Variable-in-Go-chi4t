2b4d74c1aea5b55b3a826cc2f289b2fd688a6c2f
BugHunter-X <askzenai@gmail.com>
2025-01-09 11:44:04 +0100

    Add bugSolution.go file

---
+++ bugSolution.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+
+        "fmt"
+
+)
+
+func main() {
+
+        var myVar int // Declare and initialize the variable
+
+        myVar = 10     // Assign a value
+
+        fmt.Println(myVar) // Print the value of the variable
+
+}      
